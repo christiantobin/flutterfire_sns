227f0726fce040bda90b06ca1f87b8c38864917c
Sean Park <chanseanpark@gmail.com>
2021-08-19 20:02:14 -0700

    form

---
+++ lib/device_form.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class DeviceForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return DeviceFormState();
+  }
+}
+
+class DeviceFormState extends State<DeviceForm> {
+  // Create form key
+  final formKey = GlobalKey<FormState>();
+
+  String device = '';
+  String desiredVal = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        //Pass it to children widgets
+        key: formKey,
+        child: Column(
+          children: [
+            deviceField(),
+            desiredValField(),
+            Container(
+              margin: EdgeInsets.only(top: 25.0),
+            ),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget deviceField() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'Device Name/Serial',
+      ),
+      onSaved: (String? value) {
+        if (value != null) {
+          device = value;
+        }
+      },
+    );
+  }
+
+  Widget desiredValField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Enter Value',
+      ),
+      onSaved: (String? value) {
+        if (value != null) {
+          desiredVal = value;
+        }
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      child: Text('Submit'),
+      onPressed: () async {
+        //Access form key created by its parent widget
+        if (formKey.currentState != null) {
+          formKey.currentState?.save();
+
+          // Take *both* device and desiredVal
+          // add post them to some API
+          print('This should send IoT Date to register and subscribe to');
+          var url = Uri.parse(
+              'https://0t52mixuj9.execute-api.us-east-1.amazonaws.com/prod/update');
+
+          var data = {
+            "serial": device,
+            "payload": {
+              "state": {
+                "desired": {"heatTo": int.parse(desiredVal)}
+              }
+            }
+          };
+
+          var body = convert.jsonEncode(data);
+
+          var headers = {'Content-Type': 'application/json'};
+
+          var response = await http.put(url, headers: headers, body: body);
+          print(body);
+          print('Response Status: ${response.statusCode}');
+          print('Response Body: ${response.body}');
+        }
+      },
+    );
+  }
+}
